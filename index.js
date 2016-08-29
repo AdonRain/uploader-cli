7ca0fea4d953512c8b68cb2c31f7133e54313541
雷东升 <dongsheng.lei@vhall.com>
2016-08-29 10:32:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+require('shelljs/global');
+
+var fs = require('fs');
+var path = require('path');
+var request = require('request');
+var opts = process.argv.slice(2,4);
+var flag = '--server=';
+
+opts.sort(function (a, b){
+  return a.indexOf(flag) - b.indexOf(flag);
+});
+
+var file = path.join(__dirname, opts[0]);
+var server = (opts[1].split('='))[1];
+
+request.post({
+  url: server,
+  formData: {
+    file:  fs.createReadStream(file)
+  }
+}, function(err, res, body) {
+  if(err) {
+    throw err;
+  }
+
+  var url = JSON.parse(body).data.url;
+
+  console.log(body);
+});
+
